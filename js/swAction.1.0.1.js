5304274bb544a6adc91738fb52fedf83006f3f4d
jmtdWeb <176892990@qq.com>
2017-09-05 14:02:07 +0800

    面向对象

--- js/swAction.1.0.1.js
+++ js/swAction.1.0.1.js
@@ -1,16 +1,16 @@
-class swAction {
-    /*
-     * constructor构造器
-     */
-    constructor(id){
-        this.id = id;
-        window.onscroll = ()=> this.action();
-        this.action();
-    }
-    /*
+function swAction (id){
+	this.id  = id;
+	var self = this;
+	window.onscroll = function(){
+		self.action();
+	}
+	this.action();
+}
+swAction.prototype = {
+	/*
 	 * 获取滚动条高度
 	 */
-    getScrollTop(){
+    getScrollTop : function(){
 	    var scrollTop = 0;
 	    if(document.documentElement && document.documentElement.scrollTop){
 	        var scrollTop = document.documentElement.scrollTop;
@@ -18,11 +18,12 @@ class swAction {
 	        var scrollTop = document.body.scrollTop;
 	    }
 	    return scrollTop;
-	}
+	},
+	
     /*
 	 * 取窗口可视范围的高度 
 	 */
-    getClientHeight (){
+    getClientHeight : function (){
 	    var clientHeight = 0;
 	    if(document.body.clientHeight&&document.documentElement.clientHeight){
 	        var clientHeight = (document.body.clientHeight<document.documentElement.clientHeight)?document.body.clientHeight:document.documentElement.clientHeight;        
@@ -30,69 +31,72 @@ class swAction {
 	        var clientHeight = (document.body.clientHeight>document.documentElement.clientHeight)?document.body.clientHeight:document.documentElement.clientHeight;    
 	    }
 	    return clientHeight;
-	}
+	},
     /*
 	 * hasClass
 	 */
-    hasClass (ele, cls){
+    hasClass : function (ele, cls){
 		cls = cls || '';
 		if (cls.replace(/\s/g, '').length == 0) return false; //当cls没有参数时，返回false
 		return new RegExp(' ' + cls + ' ').test(' ' + ele.className + ' ');
-	}
+	},
     /*
 	 * addClass
 	 */
-	addClass (ele, cls){
+	addClass : function (ele, cls){
 		if (!this.hasClass(ele, cls)) {
     		ele.className = ele.className == '' ? cls : ele.className + ' ' + cls;
 		}
-	}
+	},
 	/*
 	 * removeClass
 	 */
-	removeClass (ele, cls){
-		if (hasClass(elem, cls)) {
-		    let newClass = ' ' + elem.className.replace(/[\t\r\n]/g, '') + ' ';
+	removeClass : function (ele, cls){
+		if (this.hasClass(elem, cls)) {
+		    var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, '') + ' ';
 		    while (newClass.indexOf(' ' + cls + ' ') >= 0) {
 		    	newClass = newClass.replace(' ' + cls + ' ', ' ');
 		    }
 		    elem.className = newClass.replace(/^\s+|\s+$/g, '');
 		}
-	}
+	},
 	/*
 	 * 计算div到达顶部距离
 	 * 不准，jq的.offset().top也不准
 	 * 烦
 	 */
-	offTop(id, height=0){
+	offTop : function(id, height=0){
 		var classBox = id.offsetParent.offsetParent;
 		if(classBox == null){
-			let top = parseInt(id.offsetTop)+height;
+			var top = parseInt(id.offsetTop)+height;
 			//console.log('|=>'+top);
 			return top;
 		}else{
-			return this.offTop(id.offsetParent, height + id.offsetTop);//return 这个返回我理解不了
+			return this.offTop(id.offsetParent, height + id.offsetTop);
 		}		
-	}
+	},
 	/*
 	 * 处理事件
 	 */
-    action(){
-    	let height = this.getScrollTop() + this.getClientHeight();
-    	let name   = document.getElementsByClassName(this.id);
+    action : function(){
+    	var height = this.getScrollTop() + this.getClientHeight();
+    	var name   = document.getElementsByClassName(this.id);
     	for(var i=0; i<name.length; i++) {
-			//let top = name[index].offsetTop;//offsetTop相对定位之后，就计算到达有定位的父级距离	
-			let top = this.offTop(name[i]);
+			//var top = name[index].offsetTop;//offsetTop相对定位之后，就计算到达有定位的父级距离	
+			var top = this.offTop(name[i]);
 			//console.log(top);
 	        if (top < height) {
-	            let className = name[i];
-	            let effect    = className.getAttribute('swAction-effect');
-	            let duration  = className.getAttribute('swAction-duration');
-	            let delay     = className.getAttribute('swAction-delay');
-	            let nameArr   = className.getAttribute('class');
+	            var className = name[i];
+	            var effect    = className.getAttribute('swAction-effect');
+	            var duration  = className.getAttribute('swAction-duration');
+	            var delay     = className.getAttribute('swAction-delay');
+	            var nameArr   = className.getAttribute('class');
 	            this.addClass(className, effect);
 	            className.style = 'animation-duration:'+duration+'; animation-delay:'+delay; 
 	        }
 		}
     }
+    /*
+     *结束
+     */
 }
